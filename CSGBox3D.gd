9c0eec022661a028ab697faeed45ff4fee245107
10dine <ishtyaqkhan1@gmail.com>
2024-01-28 20:55:24 -0500

    new spin speed and branch

--- CSGBox3D.gd
+++ CSGBox3D.gd
@@ -1,11 +1,13 @@
 extends CSGBox3D
-
+var foo;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	foo = 0.001
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.rotate(Vector3(0, 1, 0), delta*5)
+	self.rotate(Vector3(0, 1, 0), foo)
+	foo+=0.001
